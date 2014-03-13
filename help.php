a6497a2e00d8caf540b8da77ab06ba275b6fd06a
Jwoolley <jwoolley44@hotmail.co.uk>
2014-03-13 15:17:27 +0000

    Added help.php

---
+++ help.php
@@ -0,0 +1,9 @@
+<link rel="shortcut icon" href="include/images/vf1_icon_big.ico" />
+<link id="style_link" rel=StyleSheet href="include/vf1-1024x768.css" type="text/css" media=all />
+
+<html>
+<h1><form class = menubutton name="form1" method="post" action="main_page.php">
+
+<input type="submit" name="Submit" value="Back to menu"></form>Online Help</h1>
+<p>Text</p>
+</html>
