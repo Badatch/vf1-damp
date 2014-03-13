aaca9145a517a66bfc35dc249292d641d9c4c61a
Nadeem24 <NadeemQ24@gmail.com>
2014-03-13 15:44:51 +0000

    Update help.php

--- help.php
+++ help.php
@@ -2,8 +2,8 @@
 <link id="style_link" rel=StyleSheet href="include/vf1-1024x768.css" type="text/css" media=all />
 
 <html>
-<h1><form class = menubutton name="form1" method="post" action="main_page.php">
-
+<h1 class = head2><form class = menubutton name="form1" method="post" action="main_page.php">
 <input type="submit" name="Submit" value="Back to menu"></form>Online Help</h1>
-<p>Text</p>
+
+<p>BASIC NAVIGATION: </p>
 </html>
