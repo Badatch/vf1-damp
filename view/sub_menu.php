37ce3de4f7bf53c65b3ed4873184c04a283195da
Badatch <hbadatch@gmail.com>
2014-03-15 13:15:03 +0000

    Update sub_menu.php

--- view/sub_menu.php
+++ view/sub_menu.php
@@ -13,51 +13,53 @@
 
 // let's create a context sensitive sub menu for each class of the system
 
-				if ($mode == "")
+				if ($mode == "" || $mode == "confirm_update")
 				{
 					echo "<p class='p1'>".$class_value.": 
-					<a href='". $current_file_name."?here=".$class_value."&mode=create&class_obj=".$class_value."'>create ".$class_value."</a> ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=search&class_obj=".$class_value."'>retrieve ".$class_value."</a> ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=update&class_obj=".$class_value."'>update ".$class_value."</a> ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=delete&class_obj=".$class_value."'>delete ".$class_value."</a></p>";
+					<a href='". $current_file_name."?here=".$class_value."&mode=create&class_obj=".$class_value."'>create new ".$class_value."</a> |  
+					<a href='". $current_file_name."?here=".$class_value."&mode=search&class_obj=".$class_value."'>search ".$class_value."</a> |
+					<a href='". $current_file_name."?here=".$class_value."&mode=delete&class_obj=".$class_value."'>delete ".$class_value."</a>
+					</p>";
 				}
 				else if ($mode == "create" || $mode == "confirm_create" )
 				{
 					echo "<p class='p1'>
-					<a href='". $current_file_name."?here=".$class_value."&class_obj=".$class_value."'>".$class_value."</a>: 
-					create ".$class_value." ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=search&class_obj=".$class_value."'>retrieve ".$class_value."</a> ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=update&class_obj=".$class_value."'>update ".$class_value."</a> ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=delete&class_obj=".$class_value."'>delete ".$class_value."</a></p>";
+					<a href='". $current_file_name."?here=".$class_value."&class_obj=".$class_value."'>".$class_value."</a>: create new ".$class_value." | 
+					<a href='". $current_file_name."?here=".$class_value."&mode=search&class_obj=".$class_value."'>search ".$class_value."</a> |
+					<a href='". $current_file_name."?here=".$class_value."&mode=delete&class_obj=".$class_value."'>delete ".$class_value."</a>
+					
+					</p>";
 				}
 				else if ($mode == "search" || $mode == "confirm_search")
 				{
 					echo "<p class='p1'>
-					<a href='". $current_file_name."?here=".$class_value."&class_obj=".$class_value."'>".$class_value."</a>:
-					<a href='". $current_file_name."?here=".$class_value."&mode=create&class_obj=".$class_value."'>create ".$class_value."</a> ¦ 
-					retrieve ".$class_value." ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=update&class_obj=".$class_value."'>update ".$class_value."</a> ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=delete&class_obj=".$class_value."'>delete ".$class_value."</a></p>";
+					<a href='". $current_file_name."?here=".$class_value."&class_obj=".$class_value."'>".$class_value."</a>: 
+					<a href='". $current_file_name."?here=".$class_value."&mode=create&class_obj=".$class_value."'>create new ".$class_value."</a> | search ".$class_value." |
+					<a href='". $current_file_name."?here=".$class_value."&mode=delete&class_obj=".$class_value."'>delete ".$class_value."</a> 
+					</p>";
 				}
-				else if ($mode == "update" || $mode == "confirm_update")
+				else if ($mode == "update")
 				{
 					echo "<p class='p1'>
-					<a href='". $current_file_name."?here=".$class_value."&class_obj=".$class_value."'>".$class_value."</a>:
-					<a href='". $current_file_name."?here=".$class_value."&mode=create&class_obj=".$class_value."'>create ".$class_value."</a> ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=search&class_obj=".$class_value."'>retrieve ".$class_value."</a> ¦ 
-					update ".$class_value." ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=delete&class_obj=".$class_value."'>delete ".$class_value."</a></p>";
+					<a href='". $current_file_name."?here=".$class_value."&class_obj=".$class_value."'>".$class_value."</a>: 
+					<a href='". $current_file_name."?here=".$class_value."&mode=create&class_obj=".$class_value."'>create new ".$class_value."</a> |
+					<a href='". $current_file_name."?here=".$class_value."&mode=search&class_obj=".$class_value."'>search ".$class_value."</a> |
+					<a href='". $current_file_name."?here=".$class_value."&mode=delete&class_obj=".$class_value."'>delete ".$class_value."</a> 
+					</p>";
 				}
-				else if ($mode == "delete" || $mode == "confirm_delete" )
+				else if ($mode == "delete" || $mode == "confirm_delete")
 				{
 					echo "<p class='p1'>
-					<a href='". $current_file_name."?here=".$class_value."&class_obj=".$class_value."'>".$class_value."</a>:
-					<a href='". $current_file_name."?here=".$class_value."&mode=create&class_obj=".$class_value."'>create ".$class_value."</a> ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=search&class_obj=".$class_value."'>retrieve ".$class_value."</a> ¦ 
-					<a href='". $current_file_name."?here=".$class_value."&mode=update&class_obj=".$class_value."'>update ".$class_value."</a> ¦ 
-					delete ".$class_value."</p>";
+					<a href='". $current_file_name."?here=".$class_value."&class_obj=".$class_value."'>".$class_value."</a>: 
+					<a href='". $current_file_name."?here=".$class_value."&mode=create&class_obj=".$class_value."'>create new ".$class_value."</a> | 
+					<a href='". $current_file_name."?here=".$class_value."&mode=search&class_obj=".$class_value."'>search ".$class_value."</a> |
+					<a href='". $current_file_name."?here=".$class_value."&mode=delete&class_obj=".$class_value."'>delete ".$class_value."</a>   
+					</p>";
 				}
 			}
 		}
-		
+	
+
+	
+	
 ?>	
